PixelExperience-Devices/device_xiaomi_andromeda

andromeda: Use component-override to configure Bluetooth
Runtime Resource Overlays (RROs) can no longer change the value of
resources in manifest when read during PackagerParser.

This change uses component-override to enable the service by default
on sunfish devices.

Remove usages of profile_supported_* from overlays as they no
longer configure the enable state of Bluetooth componenets.

Bug: 135048762
Test:
      adb shell pm query-services \
      com.android.bluetooth/.a2dpsink.A2dpSinkService
      adb shell pm query-services \
      com.android.bluetooth/.avrcp.AvrcpTargetService
      adb shell pm query-services \
      com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
      adb shell pm query-services \
      com.android.bluetooth/.ba.BATService
      adb shell pm query-services \
      com.android.bluetooth/.hfpclient.HeadsetClientService
      adb shell pm query-services \
      com.android.bluetooth/.hid.HidDeviceService
      adb shell pm query-services \
      com.android.bluetooth/.sap.SapService

Change-Id: If41df9fdefbcd952f8387451dc137565343e3d75

device.mk
@@ -22,8 +22,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-lineage
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
@@ -124,6 +123,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor
+
+# Component override
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Context Hub
 PRODUCT_PACKAGES += \

Havoc-Devices/android_device_xiaomi_andromeda

device.mk
@@ -22,8 +22,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-havoc \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-havoc
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
@@ -124,6 +123,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor
+
+# Component override
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Context Hub
 PRODUCT_PACKAGES += \